0d88da596a6e92add45473ff852eed393dc1ceda
Ariff-dev <Ariff.dev24@gmail.com>
2024-10-14 17:50:22 -0600

    Implement Context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/root.tsx'
+import UserProvider from './context/UserProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <UserProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </UserProvider>
   </StrictMode>
 )
