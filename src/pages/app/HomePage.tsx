38a51fe1ded897a8a93aa970c620149fdf85d37d
Ariff-dev <Ariff.dev24@gmail.com>
2024-10-16 22:50:16 -0600

    adv: Carrousel

--- src/pages/app/HomePage.tsx
+++ src/pages/app/HomePage.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 const dataPrueba = [
   {
     title: 'Inception',
@@ -26,21 +28,24 @@ const dataPrueba = [
     ImageURL:
       'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
   },
-  {
-    title: 'Pulp Fiction',
-    description: 'Un entrelazado de historias de criminales en Los Ángeles.',
-    ImageURL:
-      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
-  },
-  {
-    title: 'Pulp Fiction',
-    description: 'Un entrelazado de historias de criminales en Los Ángeles.',
-    ImageURL:
-      'https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_.jpg',
-  },
 ]
 
 export const HomePage = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const itemsToShow = 2 // Número de elementos a mostrar a la vez
+
+  const handleNext = () => {
+    if (currentIndex < dataPrueba.length - itemsToShow) {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
+
   return (
     <main className='mx-2'>
       <div className='mt-12 flex flex-col gap-8'>
@@ -49,27 +54,39 @@ export const HomePage = () => {
             <span className='font-bold'>Nuevos posts </span>de la temporada
           </h3>
           <div className='flex gap-4'>
-            <button className='bg-secondary-color-component text-xl p-2 px-4 rounded-lg'>
+            <button
+              className='bg-secondary-color-component text-xl p-2 px-4 rounded-lg'
+              onClick={handlePrev}
+              disabled={currentIndex === 0}
+            >
               <span>{'<'}</span>
             </button>
-            <button className='bg-secondary-color-component text-xl p-2 px-4 rounded-lg'>
+            <button
+              className='bg-secondary-color-component text-xl p-2 px-4 rounded-lg'
+              onClick={handleNext}
+              disabled={currentIndex >= dataPrueba.length - itemsToShow}
+            >
               <span>{'>'}</span>
             </button>
           </div>
         </div>
-        <div className='overflow-x-auto'>
-          <div className='flex flex-nowrap space-x-4 absolute'>
-            {dataPrueba.map((movie, index) => (
-              <div key={index} className='flex flex-col items-center w-48'>
-                <img
-                  src={movie.ImageURL}
-                  alt={movie.title}
-                  className='w-full h-72 object-cover rounded-lg'
-                />
-                <p className='text-center mt-2 font-semibold'>{movie.title}</p>
-                <p className='text-center text-sm'>{movie.description}</p>
-              </div>
-            ))}
+        <div className=''>
+          <div className='flex flex-nowrap items-center absolute gap-10 mx-8'>
+            {dataPrueba
+              .slice(currentIndex, currentIndex + itemsToShow)
+              .map((movie, index) => (
+                <div key={index} className='flex flex-col items-center w-48'>
+                  <img
+                    src={movie.ImageURL}
+                    alt={movie.title}
+                    className='w-full h-72 object-cover rounded-lg'
+                  />
+                  <p className='text-center mt-2 font-semibold'>
+                    {movie.title}
+                  </p>
+                  <p className='text-center text-sm'>{movie.description}</p>
+                </div>
+              ))}
           </div>
         </div>
       </div>
