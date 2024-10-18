d8b907eb5897d5c05512a7d5f6991884e550c85d
Ariff-dev <Ariff.dev24@gmail.com>
2024-10-17 23:10:03 -0600

    rf: Card COmponent Post

--- src/components/home/HomeCarrousel.tsx
+++ src/components/home/HomeCarrousel.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import HomePrimaryCard from './HomePrimaryCard'
 
 const dataPrueba = [
   {
@@ -95,15 +96,7 @@ export default function HomeCarrousel() {
           {dataPrueba
             .slice(currentIndex, currentIndex + itemsToShow)
             .map((movie, index) => (
-              <div key={index} className='flex flex-col items-center w-full'>
-                <img
-                  src={movie.ImageURL}
-                  alt={movie.title}
-                  className='w-full h-72 object-cover rounded-lg'
-                />
-                <p className='text-center mt-2 font-semibold'>{movie.title}</p>
-                <p className='text-center text-sm'>{movie.description}</p>
-              </div>
+              <HomePrimaryCard movie={movie} index={index} />
             ))}
         </div>
       </div>
--- src/components/home/HomePrimaryCard.tsx
+++ src/components/home/HomePrimaryCard.tsx
@@ -1,13 +1,24 @@
-interface MovieDesc {
+interface Movie {
+  ImageURL: string
   title: string
   description: string
-  imageURL: string
 }
 
-export default function HomePrimaryCard({
-  title,
-  description,
-  imageURL,
-}: MovieDesc) {
-  return <div></div>
+interface MovieDesc {
+  movie: Movie
+  index: number
+}
+
+export default function HomePrimaryCard({ movie, index }: MovieDesc) {
+  return (
+    <div key={index} className='flex flex-col items-center w-full'>
+      <img
+        src={movie.ImageURL}
+        alt={movie.title}
+        className='w-full h-72 object-cover rounded-lg'
+      />
+      <p className='text-center mt-2 font-semibold'>{movie.title}</p>
+      <p className='text-center text-sm'>{movie.description}</p>
+    </div>
+  )
 }
